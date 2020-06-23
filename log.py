d73f7b61f6adf308273850a1d14b86d6182c664c
arnabroy28ju <46686291+arnabroy28ju@users.noreply.github.com>
2020-06-23 16:53:47 +0530

    Add files via upload

---
+++ log.py
@@ -0,0 +1,8 @@
+#writing logs in file
+import logging
+def set_custom_log_info(filename):
+    logging.basicConfig(filename=filename,
+    level=logging.INFO)
+
+def report(e:Exception):
+    logging.exception(str(e))
